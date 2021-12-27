dbfa9ebdaa5062e4db74d67160dafb1ac5fb8ebb
Neal John <neiljohnson777@gmail.com>
2021-12-27 18:32:55 +0400

    module4

---
+++ module4/SCRIPT.js
@@ -0,0 +1,17 @@
+(function () {
+
+    var names = ["Neal", "John", "Anand", "Soumya", "Lesly", "Aryan", "Ethan", "Ishaan", "Johny", "Rindu"];
+    
+    for (var i = 0; i < names.length; i++) {
+      var firstLetter = names[i].charAt(0).toLowerCase();
+    
+      if (firstLetter === 'j') {
+        byeSpeaker(names[i]);
+      }
+      else {
+        helloSpeaker(names[i]);
+      }
+    }
+    
+    })();
+    
